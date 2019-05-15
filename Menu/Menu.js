c5a659b7fde333e6a153007f562a459f85fd833a
Donald Whitely <dswhitely1@gmail.com>
2019-05-15 14:48:55 -0400

    Menu animation done.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,8 +1,8 @@
-
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-menu.classList.toggle('menu--open')
-}
+	// Toggle the "menu--open" class on your menu refence.
+	menu.classList.toggle('menu--open');
+	menu.classList.contains('menu--open') ? TweenMax.to('.menu--open', 2, { x: 350 }) : TweenMax.to('.menu', 2, { x: 0 });
+};
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
@@ -10,3 +10,16 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+const theWholeDocument = document.querySelectorAll('divs');
+
+theWholeDocument.forEach(element =>
+	element.addEventListener('click', e => {
+		e.stopPropagation();
+		console.log(e.target);
+		if (menu.classList.contains('menu--open')) {
+			menu.classList.toggle('menu-open');
+			TweenMax('.menu', 2, { x: 0 });
+		}
+	}),
+);
