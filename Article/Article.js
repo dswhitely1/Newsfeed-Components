d10722b8fab7eb7fe54824c55dc0b428e9a658d2
Donald Whitely <dswhitely1@gmail.com>
2019-05-15 11:30:25 -0400

    Article.js finished

--- Article/Article.js
+++ Article/Article.js
@@ -1,23 +1,22 @@
-// Because classes are not hoisted you will need to start your code at the bottom of the page.  
+// Because classes are not hoisted you will need to start your code at the bottom of the page.
 // Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = this.domElement.querySelector('.expandButton');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent="Expand";
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
-  }
+	constructor(domElement) {
+		// assign this.domElement to the passed in domElement
+		this.domElement = domElement;
+		// create a reference to the ".expandButton" class.
+		this.expandButton = this.domElement.querySelector('.expandButton');
+		// Using your expandButton reference, update the text on your expandButton to say "expand"
+		this.expandButton.textContent = 'Expand';
+		// Set a click handler on the expandButton reference, calling the expandArticle method.
+		this.expandButton.addEventListener('click', () => this.expandArticle());
+	}
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article--open')
-    // this.domElement.classList.toggle('close');
-  }
+	expandArticle() {
+		// Using our reference to the domElement, toggle a class to expand or hide the article.
+		console.log(this.domElement.classList);
+	}
 }
 
 /* START HERE: 
@@ -31,4 +30,6 @@ class Article {
 
 let articles = document.querySelectorAll('.article');
 
-articles.forEach(article => { return new Article(article)});
+articles.forEach(article => {
+	return new Article(article);
+});
